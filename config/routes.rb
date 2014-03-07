2297b0cb0dd05e2df1e223652555a5f6996c62b0
kou <kou@WebServerTest.(none)>
2014-03-07 16:24:10 +0900

    Add Message

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Task::Application.routes.draw do
+  root "main_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
